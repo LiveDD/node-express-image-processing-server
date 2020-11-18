3f222dc04fcec49020d5e1106fedfc5900f73784
Kirti Kumar Sharma <kishar@microsoft.com>
2020-11-18 21:38:27 +0530

    Module 4

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const multer = require('multer');
 const path = require('path');
+const imageProcessor = require('./imageProcessor');
 
 const router = Router();
 
@@ -27,10 +28,13 @@ const upload = multer({
   storage: storage,
 });
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
   if (request.fileValidationError) {
     response.status(400).json({error: request.fileValidationError});
   } else {
+    try {
+      await imageProcessor(request.file.filename);
+    } catch (e) {}
     response.status(201).json({success: true});
   }
 });
